145e2f7bbe1d352b9c8114ea49048e84fb017712
Steven Young <ywch2002@126.com>
2022-11-18 11:57:17 +0800

    lexical_analysis

--- lexer.c
+++ lexer.c
@@ -5,69 +5,74 @@
 /* Kenneth C. Louden                                */
 /****************************************************/
 
+#include "lexer.h"
 #include "globals.h"
 #include "util.h"
-#include "lexer.h"
 
 /* states in lexer DFA */
-typedef enum
-   { START,INASSIGN,INCOMMENT,INNUM,INID,DONE }
-   StateType;
+typedef enum { START, INASSIGN, INCOMMENT, INNUM, INID, DONE } StateType;
 
 /* lexeme of identifier or reserved word */
-char tokenString[MAXTOKENLEN+1];
+char tokenString[MAXTOKENLEN + 1];
 
 /* BUFLEN = length of the input buffer for
    source code lines */
 #define BUFLEN 256
 
 static char lineBuf[BUFLEN]; /* holds the current line */
-static int linepos = 0; /* current position in LineBuf */
-static int bufsize = 0; /* current size of buffer string */
+static int linepos = 0;      /* current position in LineBuf */
+static int bufsize = 0;      /* current size of buffer string */
 static int EOF_flag = FALSE; /* corrects ungetNextChar behavior on EOF */
 
 /* getNextChar fetches the next non-blank character
    from lineBuf, reading in a new line if lineBuf is
    exhausted */
-int getNextChar(void)
-{ if (!(linepos < bufsize))
-  { lineno++;
-    if (fgets(lineBuf,BUFLEN-1,source))
-    { if (EchoSource) fprintf(listing,"%4d: %s",lineno,lineBuf);
-      bufsize = strlen(lineBuf);
-      linepos = 0;
-      return lineBuf[linepos++];
-    }
-    else
-    { EOF_flag = TRUE;
-      return EOF;
-    }
-  }
-  else return lineBuf[linepos++];
+int getNextChar(void) {
+    if (!(linepos < bufsize)) {
+        // we add the line number if the last line ends with '\n'
+        if (lineBuf[linepos - 1] == '\n') {
+            lineno++;
+        }
+        if (fgets(lineBuf, BUFLEN - 1, source)) {
+            // lineno is 0 when we find the lineBuf is not NULL
+            if (lineno == 0)
+                lineno++;
+            bufsize = strlen(lineBuf);
+            linepos = 0;
+            if (EchoSource)
+                fprintf(listing, "%4d: %s", lineno, lineBuf);
+            return lineBuf[linepos++];
+        } else {
+            EOF_flag = TRUE;
+            return EOF;
+        }
+    } else
+        return lineBuf[linepos++];
 }
 
 /* ungetNextChar backtracks one character
    in lineBuf */
-static void ungetNextChar(void)
-{ if (!EOF_flag) linepos-- ;}
+static void ungetNextChar(void) {
+    if (!EOF_flag)
+        linepos--;
+}
 
 /* lookup table of reserved words */
-static struct
-    { char* str;
-      TokenType tok;
-    } reservedWords[MAXRESERVED]
-   = {{"if",IF},{"then",THEN},{"else",ELSE},{"end",END},
-      {"repeat",REPEAT},{"until",UNTIL},{"read",READ},
-      {"write",WRITE}};
+static struct {
+    char* str;
+    TokenType tok;
+} reservedWords[MAXRESERVED] = {
+    {"if", IF},         {"then", THEN},   {"else", ELSE}, {"end", END},
+    {"repeat", REPEAT}, {"until", UNTIL}, {"read", READ}, {"write", WRITE}};
 
 /* lookup an identifier to see if it is a reserved word */
 /* uses linear search */
-static TokenType reservedLookup (char * s)
-{ int i;
-  for (i=0;i<MAXRESERVED;i++)
-    if (!strcmp(s,reservedWords[i].str))
-      return reservedWords[i].tok;
-  return ID;
+static TokenType reservedLookup(char* s) {
+    int i;
+    for (i = 0; i < MAXRESERVED; i++)
+        if (!strcmp(s, reservedWords[i].str))
+            return reservedWords[i].tok;
+    return ID;
 }
 
 /****************************************/
@@ -76,56 +81,118 @@ static TokenType reservedLookup (char * s)
 /* function getToken returns the
  * next token in source file
  */
-TokenType getToken(void)
-{  /* Initialization: */
-   /* index for storing into tokenString */
-   int tokenStringIndex = 0;
-   /* holds current token to be returned */
-   TokenType currentToken;
-   /* current state - always begins at START */
-   StateType state = START;
-   /* flag to indicate save to tokenString */
-   int save;
+TokenType getToken(void) { /* Initialization: */
+    /* index for storing into tokenString */
+    int tokenStringIndex = 0;
+    /* holds current token to be returned */
+    TokenType currentToken;
+    /* current state - always begins at START */
+    StateType state = START;
+    /* flag to indicate save to tokenString */
+    int save;
 
-   /* The while loop simulates the process of DFA:
-      get one lexical unit each time,
-      update the currentToken/state/save above
-	    according to current state and the received lexical unit. 
-   */
-   while (state != DONE)
-   { int c = getNextChar();
-     save = TRUE;
-     switch (state)
-     { 
-      /* Write the process of other cases referring to case INID. */
-       case INID:
-         if (!isalpha(c))
-         { /* backup in the input */
-           ungetNextChar();
-           save = FALSE;
-           state = DONE;
-           currentToken = ID;
-         }
-         break;
+    /* The while loop simulates the process of DFA:
+        get one lexical unit each time,
+        update the currentToken/state/save above
+              according to current state and the received lexical unit.
+    */
+    while (state != DONE) {
+        int c = getNextChar();
+        save = TRUE;
+        switch (state) {
+            /* Write the process of other cases referring to case INID. */
+            case START:
+                if (isspace(c)) {
+                    save = FALSE;
+                } else if (c == '{') {
+                    save = FALSE;
+                    state = INCOMMENT;
+                } else if (isalpha(c)) {
+                    state = INID;
+                } else if (isdigit(c)) {
+                    state = INNUM;
+                } else if (c == ':') {
+                    state = INASSIGN;
+                } else if (c == '=') {
+                    state = DONE;
+                    currentToken = EQ;
+                } else if (c == '<') {
+                    state = DONE;
+                    currentToken = LT;
+                } else if (c == '+') {
+                    state = DONE;
+                    currentToken = PLUS;
+                } else if (c == '-') {
+                    state = DONE;
+                    currentToken = MINUS;
+                } else if (c == '*') {
+                    state = DONE;
+                    currentToken = TIMES;
+                } else if (c == '/') {
+                    state = DONE;
+                    currentToken = OVER;
+                } else if (c == '(') {
+                    state = DONE;
+                    currentToken = LPAREN;
+                } else if (c == ')') {
+                    state = DONE;
+                    currentToken = RPAREN;
+                } else if (c == ';') {
+                    state = DONE;
+                    currentToken = SEMI;
+                } else if (c == EOF) {
+                    state = DONE;
+                    save = FALSE;
+                    currentToken = ENDFILE;
+                } else {
+                    state = DONE;
+                    save = FALSE;
+                    currentToken = ERROR;
+                }
+                break;
+            case INCOMMENT:
+                save = FALSE;
+                if (c == '}') {
+                    state = START;
+                }
+                break;
+            case INID:
+                if (!isalpha(c)) {
+                    ungetNextChar();
+                    save = FALSE;
+                    state = DONE;
+                    currentToken = ID;
+                }
+                break;
+            case INNUM:
+                if (!isdigit(c)) {
+                    ungetNextChar();
+                    save = FALSE;
+                    state = DONE;
+                    currentToken = NUM;
+                }
+                break;
+            case INASSIGN:
+                if (c != '=') {
+                    ungetNextChar();
+                    save = FALSE;
+                    state = DONE;
+                    currentToken = ASSIGN;
+                }
+                break;
+        }
+        if ((save) && (tokenStringIndex <= MAXTOKENLEN))
+            tokenString[tokenStringIndex++] = (char)c;
+        if (state == DONE) {
+            tokenString[tokenStringIndex] = '\0';
+            if (currentToken == ID)
+                currentToken = reservedLookup(tokenString);
+        }
+    }
+    if (TraceLex) {
+        fprintf(listing, "\t%d: ", lineno);
+        printToken(currentToken, tokenString);
+    }
+    return currentToken;
 
-
-
-         
-       
-     }
-     if ((save) && (tokenStringIndex <= MAXTOKENLEN))
-       tokenString[tokenStringIndex++] = (char) c;
-     if (state == DONE)
-     { tokenString[tokenStringIndex] = '\0';
-       if (currentToken == ID)
-         currentToken = reservedLookup(tokenString);
-     }
-   }
-   if (TraceLex) {
-     fprintf(listing,"\t%d: ",lineno);
-     printToken(currentToken,tokenString);
-   }
-   return currentToken;
-
-  
 } /* end getToken */
